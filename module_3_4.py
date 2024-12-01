b31c7294ee827783527ac9a3264c7b637e0dddd9
fertmi <fertmi@mail.ru>
2024-12-01 12:47:18 +0300

    Задача "Однокоренные"

--- module_3_4.py
+++ module_3_4.py
@@ -6,5 +6,11 @@ def single_root_words(root_word, *other_words):
 #Основная программа
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'RicHies')
 result2 = single_root_words('Disablement', 'ABLe', 'Mable', 'DisabLe', 'Bagel')
+result3 = single_root_words('Рот','Рот', 'ротик', 'ворОтник', 'роТище', 'воРота')
+result4 = single_root_words('Плат', 'платный', 'расплата', 'платьишко', 'выплатить', 'зарплата', 'платьице')
+result5 = single_root_words('Кормушка', 'Птица', 'корм', 'мушка', 'Крот')
 print(result1)
 print(result2)
+print(result3)
+print(result4)
+print(result5)
