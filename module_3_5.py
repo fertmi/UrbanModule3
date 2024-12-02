664d74b5b073ef45ba2a8b33b3c8fbf68fc9ce54
fertmi <fertmi@mail.ru>
2024-12-02 08:55:50 +0300

    Задача "Рекурсивное умножение цифр"

---
+++ module_3_5.py
@@ -0,0 +1,16 @@
+#Функция рекурсивное умножение цифр
+def get_multiplied_digits(number):
+    str_number = str(number)        # перевод числа в строку
+    first = int(str_number[0])      # first присваиваем первую цифру
+    if len(str_number) > 1:         # если длина числа > 1
+        return first * get_multiplied_digits(int(str_number[1:])) # вызываем функцию со срезом: со второго числа и умножаем на первое
+    else:
+        return first                # если осталась 1 цифра возвращаем это число
+
+#Основная программа
+result = get_multiplied_digits(40203)
+print(result)
+result2 = get_multiplied_digits(402030)
+print(result2)
+result3 = get_multiplied_digits(1000040010005)
+print(result3)
