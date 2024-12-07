e46ef84f17e20d40644e97db459372f5248fad87
fertmi <fertmi@mail.ru>
2024-12-07 23:23:38 +0300

    Задание "Раз, два, три, четыре, пять .... Это не всё?" ver 2

--- module_3_6.py
+++ module_3_6.py
@@ -1,15 +1,16 @@
 #Функция - универсальное решение для подсчёта суммы всех чисел и длин всех строк
-def calculate_structure_sum(structure, sum):
+def calculate_structure_sum(structure):
+    sum = 0
     for item in structure:
         if isinstance(item, str): #Если строка
            sum += len(item)
         elif isinstance(item, int): #Если целое число
             sum += item
         elif isinstance(item, (list, tuple, set)): #Если список, кортеж, множество
-            sum = calculate_structure_sum(item,sum)
+            sum += calculate_structure_sum(item)
         elif isinstance(item, dict): #Если словарь
-            sum = calculate_structure_sum(item.keys(),sum)
-            sum = calculate_structure_sum(item.values(),sum)
+            sum += calculate_structure_sum(item.keys())
+            sum += calculate_structure_sum(item.values())
     return sum
 
 #Основная программа
@@ -20,5 +21,5 @@ data_structure = [
 "Hello",
 ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-result = calculate_structure_sum(data_structure,0)
+result = calculate_structure_sum(data_structure)
 print(result)
